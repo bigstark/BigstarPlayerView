b6539407e9b19f918d5af369c5911b64fd911271
BigStarK <bigstark@teletalkvi.com>
2014-07-19 15:57:35 +0900

    Remove SeekBarHandler, combine with MeidaControllerHandler

--- src/com/bigstark/controller/BigstarPlayerView.java
+++ src/com/bigstark/controller/BigstarPlayerView.java
@@ -1,21 +1,60 @@
 package com.bigstark.controller;
 
 import android.content.Context;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.AttributeSet;
+import android.widget.FrameLayout;
 import android.widget.VideoView;
 
-public class BigstarPlayerView extends VideoView {
+public class BigstarPlayerView extends FrameLayout {
 	
+	private VideoView videoView;
+
 	public BigstarPlayerView(Context context) {
 		this(context, null);
 	}
-	
+
 	public BigstarPlayerView(Context context, AttributeSet attrs) {
 		this(context, attrs, 0);
 	}
 
 	public BigstarPlayerView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
+	}
+	
+	public VideoView getVideoView() {
+		return videoView;
+	}
+
+	static class SavedState extends BaseSavedState {
+		int stateToSave;
+
+		SavedState(Parcelable superState) {
+			super(superState);
+		}
+
+		private SavedState(Parcel in) {
+			super(in);
+			this.stateToSave = in.readInt();
+		}
+
+		@Override
+		public void writeToParcel(Parcel out, int flags) {
+			super.writeToParcel(out, flags);
+			out.writeInt(this.stateToSave);
+		}
+
+		// required field that makes Parcelables from a Parcel
+		public static final Parcelable.Creator<SavedState> CREATOR = new Parcelable.Creator<SavedState>() {
+			public SavedState createFromParcel(Parcel in) {
+				return new SavedState(in);
+			}
+
+			public SavedState[] newArray(int size) {
+				return new SavedState[size];
+			}
+		};
 	}
 
 }
--- src/com/bigstark/controller/MediaControllerHandler.java
+++ src/com/bigstark/controller/MediaControllerHandler.java
@@ -1,5 +1,100 @@
 package com.bigstark.controller;
 
-public class MediaControllerHandler {
+import android.os.Handler;
+import android.os.Message;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+class MediaControllerHandler extends Handler {
+	
+	private VideoView videoView;
+	
+	private TextView tvCurrentPosition;
+	private SeekBar seekBar;
+	private ImageView ivPlayPause;
+	
+	boolean isPlaying = false;
+	private int currentPosition = 0;
+	
+	public MediaControllerHandler(BigstarPlayerView videoPlayerView) {
+		this.videoView = videoPlayerView.getVideoView();
+		
+	}
+	
+	public void setCurrentPosition(TextView tvCurrentPosition) {
+		this.tvCurrentPosition = tvCurrentPosition;
+	}
+	
+	public void setSeekBar(SeekBar seekBar) {
+		this.seekBar = seekBar;
+	}
+	
+	public void setPlayPause(ImageView ivPlayPause) {
+		this.ivPlayPause = ivPlayPause;
+	}
+
+	@Override
+	public void handleMessage(Message msg) {
+		super.handleMessage(msg);
+		
+		if(videoView == null) {
+			return;
+		}
+
+		if (!isPlaying) {
+			return;
+		}
+
+		currentPosition = videoView.getCurrentPosition() / 1000;
+
+		int minute = currentPosition / 60;
+		int second = currentPosition - (minute * 60);
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(String.format("%d", minute) + ":");
+		sb.append(String.format("%02d", second));
+		tvCurrentPosition.setText("" + sb.toString());
+		seekBar.setProgress(currentPosition);
+
+		sendEmptyMessageDelayed(0, 1000);
+	}
+
+	public void start() {
+		if (videoView == null) {
+			return;
+		}
+
+		isPlaying = true;
+		videoView.start();
+		ivPlayPause.setImageResource(R.drawable.ic_pause_circle);
+		sendEmptyMessage(0);
+	}
+
+	public void pause() {
+		if (videoView == null) {
+			return;
+		}
+
+		isPlaying = false;
+		videoView.pause();
+		ivPlayPause.setImageResource(R.drawable.ic_play_circle);
+	}
+
+	/**
+	 * @param second
+	 *  not millisecond
+	 */
+	public void seekTo(int second) {
+		if (videoView == null) {
+			return;
+		}
+		videoView.seekTo(second * 1000);
+	}
+
+	public int getCurrentPosition() {
+		return currentPosition;
+	}
 
 }
