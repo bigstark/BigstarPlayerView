d71dd6026688afca7e276b8394c96b1552fa1503
BigStarK <bigstark@makeus.com>
2016-03-15 17:28:56 +0900

    update video view and samples

--- Sample/src/main/java/com/bigstark/controller/sample/PlayerActivity.java
+++ Sample/src/main/java/com/bigstark/controller/sample/PlayerActivity.java
@@ -4,12 +4,16 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 
 import com.bigstark.controller.BigstarPlayerView;
+import com.bigstark.video.ErrorReason;
+import com.bigstark.video.OnPlayStateChangedListener;
+import com.bigstark.video.OnPlaybackEventListener;
 
 
 public class PlayerActivity extends Activity {
-    private static final String SAMPLE_URL = "http://api.wecandeo.com/video/default/BOKNS9AQWrFSTupSbNE3M60gpF3OhwV3K8yrlKIEcwYPMWo7Gbip7cgieie";
+    private static final String SAMPLE_URL = "https://walterebert.com/playground/video/hls/sintel-trailer.m3u8";
 
     private final float VIDEO_RATIO = (float) 9 / (float) 16;
     private final String KEY_CURRENT_POSITION = "CurrentPosition";
@@ -36,6 +40,57 @@ public class PlayerActivity extends Activity {
 
     private void initVideo() {
         playerView = (BigstarPlayerView) findViewById(R.id.bigstar_player_view);
+        playerView.setOnPlayStateChangedListener(new OnPlayStateChangedListener() {
+            @Override
+            public void onPrepared() {
+                Log.v("TAG", "onPrepared");
+            }
+
+            @Override
+            public void onError(ErrorReason errorReason) {
+                Log.v("TAG", "onError");
+            }
+
+            @Override
+            public void onCompletion() {
+                Log.v("TAG", "onCompletion");
+            }
+
+            @Override
+            public void onReleased() {
+                Log.v("TAG", "onReleased");
+            }
+        });
+        playerView.setOnPlaybackEventListener(new OnPlaybackEventListener() {
+            @Override
+            public void onPlaying() {
+                Log.v("TAG", "onPlaying");
+            }
+
+            @Override
+            public void onPaused() {
+                Log.v("TAG", "onPaused");
+            }
+
+            @Override
+            public void onStopped() {
+                Log.v("TAG", "onStopped");
+            }
+
+            @Override
+            public void onPositionChanged(int i) {
+                Log.v("TAG", "onPosition Changed to " + i);
+            }
+
+            @Override
+            public void onBufferingUpdate(int i) {
+            }
+
+            @Override
+            public void onSeekComplete() {
+                Log.v("TAG", "onSeekComplete");
+            }
+        });
 
 //    DisplayMetrics metrics = new DisplayMetrics();
 //    getWindowManager().getDefaultDisplay().getMetrics(metrics);
